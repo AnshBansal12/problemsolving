fe32d14a953a70e3445ddb3a0f280ee8b0c7d0cb
AnshBansal12 <136993340+AnshBansal12@users.noreply.github.com>
2024-08-02 21:32:16 +0530

    Question1
    description

---
+++ src/Question1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Question1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Input your name:");
+        String name = sc.next();
+        System.out.println(name);
+
+
+    }
+
+}
