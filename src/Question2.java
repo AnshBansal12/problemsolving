5f6553f38861e9ee773f98c3d64b8a3aaa5f391c
Ansh Bansal <136993340+AnshBansal12@users.noreply.github.com>
2024-08-03 22:37:20 +0530

    Question2.java

--- src/Question2.java
+++ src/Question2.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Question2 {
-}
+import java.util.Scanner;
+public class Question2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Input how many times you have to print hello world:");
+        int n = sc.nextInt();
+        for(int i=0; i<n;i=i+1)
+            System.out.println("Hello World");
+
+
+       }
+
+
+
+
+    }
+
+
