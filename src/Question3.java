b03b9c2599704c98486a089d6106b9f03d3d35ab
AnshBansal12 <136993340+AnshBansal12@users.noreply.github.com>
2024-08-03 21:10:40 +0530

    Question3
    description

--- src/Question3.java
+++ src/Question3.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Question3 {
+import java.util.Scanner;
+public class Question3{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Input any number to obtain table 1-10:");
+        int n = sc.nextInt();
+        for(int i=1;
+            i<=10;
+            i=i+1)
+            System.out.println(n + " * " + i
+                    + " = " + n * i);
+
+
+        System.out.println(n);
+
+
+    }
+
 }
