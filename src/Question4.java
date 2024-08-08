6aac90f00fc6f8af50b20d80d644d56699ab7294
Ansh Bansal <136993340+AnshBansal12@users.noreply.github.com>
2024-08-08 20:23:05 +0530

    Create Question4.java
    I have taken input from user instead of taking a default word, and tried to do it with a different syntax.

---
+++ src/Question4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Question4{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Input word you want to check for palindrome:");
+        String n = sc.next();
+        String str = n;
+
+        String reverse = (new StringBuilder(str)).reverse().toString();
+        
+        if(str.equals(reverse)) {
+            System.out.println(str+" is a Palindrome.");
+        } else {
+            System.out.println(str+" is not a Palindrome.");
+        }
+    }}
+
+
